4dbec468bc8069372273520db53868ef7a5f46ff
Rodrigo Cesar <rodrigocesarrcf.94@gmail.com>
2022-01-13 16:20:59 -0300

    Limpeza dos comentários

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,9 @@
-
-/* 
-Case Sensitive = JS reconhece letras minusculas e maiusculas, portanto deve ser escrito com as palavras reservadas de forma correta senão não funciona. Não esquecer do argumento destas funções()
-
-/ referencie o caminho do input no qual esse nome entra, lembrando da questão da árvore DOM.
-// declarando as variáveis colocando seu imput de referencia, para validação das entradas.
-// no query selector, se for classe referencie .nomedaclasse , se for um id #id, senão não funciona. 
-
-por tag: getElementByTagName()
-por id: getElementById()
-por Nome: getElementByName()
-por classe: getElementByClassName()
-por seletor: querySelector()
-*/ 
-
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
 let mapa = document.querySelector('#mapa')
 let ODS4 = document.querySelector('#ODS4')
 
-// variaveis para verificar preenchimento de todos os campos
 let nomeOk = false
 let emailOk = false
 let assuntoOk = false
@@ -38,16 +22,14 @@ if(nome.value.length < 3){
  else{
  txtNome.innerHTML='Tamanho de Nome OK'
  txtNome.style.color='green'
- nomeOk = true // condição else, quer dizer que está correto, então boolean nome = true.
+ nomeOk = true 
  }
 }   
 
 txt.style.color='black'
 function validaEmail(){
 let txtEmail = document.querySelector('#txtEmail')
-// o indexOf verifica se o @ está ou não dentro do input e nos retorna -1 para NÃO/ 1 para SIM
-// Desta forma, chamando o email.value(Meu input), e solicitando ao indexOf se tem @ e . //
-// Ou seja, se for -1, não tem @ OU-->(indicado com ||) .//
+
 if(email.value.indexOf('@')==-1 || email.value.indexOf('.')== -1){
     txtEmail.innerHTML='E-mail inválido'
     txtEmail.style.color='red'
@@ -58,9 +40,7 @@ else{
     emailOk = true
 }
 }
-
 function validaAssunto(){
-// o id da querySelector AQUI é o id da sua div, que vai dar a mensgem pro usuário obedecendo a condiçõ do if;
     let txtAssunto = document.querySelector('#txtAssunto')
 
     if(assunto.value.length >  100){
@@ -83,14 +63,12 @@ function enviar(){
     alert('Prencha os campos corretamente')    
     }
 }
-
 function mapaZoom(){
     ODS4.style.width='800px'
     ODS4.style.height='600px'
 
 
 }
-
 function mapaNormal(){
     mapa.style.width='400px'
     mapa.style.height='250px'
